e13e5aa58448d7d04582a2f99eefb1efc1034111
gon <y2gon2@gmail.com>
2023-09-15 16:23:06 +0900

    #17 - 게시판 뷰 - Article Controller 및 해당 view index.html 파일 작성
    1. Aritcle Controller : 기존에 빈 class 에 controller 작동을 위한 요소 구현
     - @RequestMapping("/articles")   client 요청 uri 에 대한 mapping annotation
     - @Controller : Controller class 임을 명시
     - @GetMapping : mapping 되어야 할 attribute 구현을 담을 method annotation
     -  map.addAttribute("artricles", List.of()); :  attribute key - value  명시  (현재 value List 는 빈 상태)
     -  mapping view 가 있는 경로 String return
    
    2. index.html
     - HTML semantic tag 에 맞춰 작성  (design 요소는 배제함.)
     - attribute value 가 빈 list 이므로 그냥 html 상에서 수동으로 값 입력하여 table 생성
    
    3. 해당 test 활성화 및 내용 추가
     - "/articles" request 에 대한 controller - view 가 작성되었으므로 해당 test 활성화
     -  content().contentType(MediaType.TEXT_HTML) 에 대해서  UTF-8 charset 이 현재 page 는 포함되어 있어 exact matching 으로는 test 를 통과하지 못함.
     - 따라서  해당 methode 를 contentTypeCompatibleWith() 로 변경하여 포함된 조건에 대한 통과 method 로 변

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -23,7 +23,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("TODO")
+//    @Disabled("TODO")
     @DisplayName("[view][GET] 게시글 리스트 (게시판 페이지) - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsAritclesView() throws Exception{
@@ -32,7 +32,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(MockMvcRequestBuilders.get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles")); // 서버에서 게시글 목록을 받았을 view 로 articles attribute 이 전달되었는지 확인
     }
