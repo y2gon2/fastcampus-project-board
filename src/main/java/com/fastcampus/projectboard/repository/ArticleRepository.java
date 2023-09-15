e8a8913fca02e979bfbafc97802fc4e2aa1ec958
gon <y2gon2@gmail.com>
2023-09-15 12:07:36 +0900

    #6 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    1.  QuerydslPredicateExecutor<T> 상속 : <T> class 내 모든 field 에 대한 exact match 검색 기능을 추가
    2. QuerydslBinderCustomizer<Qclass> 상속 : <T> 에 대한 Qclass  field 들에 대해 interface class 에서 정의한 내용으로 상세 검색 규칙을 추가
    
    3.  java8 ? 에서 추가된 기능인 interface method 에 내용을 추가할 수 있는 기능을 사용하여 , binding  규칙을 추가
     - 기본 QuerydslPredicatedExecutor 에 의한 exact match 기능을   exclude 함.
    -  검색을 허용할 field 를 including
    -  각 항목별로 어떤 검색 조건을 넣어 줄 것인가를 설정
        StringExpression  의 경우 likeIgnore method 를 사용시 like '${value}' 로 생성 containsIgnore method 를 사용하면 like '%${value}%' 와 같이 % wildcard 가 자동을 붙음. 검색 기능에 추가적인 customazing 을 진행하지 않을 것이므로 구현의 단순화를 위해 자동 wild card 를 생성하는 containsIngnore method 를 사용함.

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,35 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // <T> class 의 모등 field 의 기본 검색기능을 추가함
+        QuerydslBinderCustomizer<QArticle>  // interface mathod 로 상세 검색 규칙을 추가할 수 있게 만듬.
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true); // 기본 검색 모든 field 검색 기능을 가진 상태를 다시 제외 시키고 
+        bindings.including(root.title, root.hashtag, root.content, root.createdAt, root.createdBy); // 검색을 원하는 field 만 다시 추가 시킴
+
+        // 대소문자 구분없는 부분 글자 검색
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);  // Query 생성이  like '${value}' -> 사용자가 % wild card 를 넣어 줘야 함.
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);  //            like '%${value}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
+
+// QuerydslPredicateExecutor<Article> 을 하면 위 설명대로 기본 검색기능이 포함되지만 다음의 한계가 존재
+// 1. 부분 검색 불가. exact match 만 검색 가능함.
