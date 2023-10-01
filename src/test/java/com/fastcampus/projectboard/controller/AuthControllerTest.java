704d5b05036e0942cbd17db36d9ca62e4d9158c1
gon <y2gon2@gmail.com>
2023-10-01 10:51:23 +0900

    #51 - AuthControllerTest: Void.class 표현 개선
    원래 해당 자리에는 아무 class 가 아닌 (empty) bean component 가 들어가야 한다.
    'void' 는 spring bean 이 아니므로 정확한 표현이 아니고
    일부 개발 환경에서 error 를 발생시킬 수 있어 test 용 empty bean 을 생성하여 넣음.
    
    해당 test bean 은 어떤 작업도 포함하지 않기 때문에 내용이 없으며, 다른 test 에서
    재활용 될 가능성이 적음을 고려하여 inner class 로 작성

--- src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Disabled
 @DisplayName("View controller - 인증 ")
 @Import(SecurityConfig.class)       // 모든 page 에 대한 url 접근이 가능하도록 설정한 config 를 import 해줘서, 기존의  get test 들이 통과하도록 변경
-@WebMvcTest
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -35,4 +36,8 @@ class AuthControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
 
     }
+
+    // 어떤 컨트롤러도 필요하지 않은 test 임을 나타내기 위해 test 용 빈 component 를 사용.
+    @TestComponent
+    static class EmptyController {}
 }
