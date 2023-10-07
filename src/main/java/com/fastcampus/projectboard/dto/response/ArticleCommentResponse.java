870066ed950ebe27d898e449b8442fb7ff62bd2f
gon <y2gon2@gmail.com>
2023-10-07 11:56:42 +0900

    #80 - 대댓글 비시니스 로직 구현
    ArticleCommentDto / ArticleCommentRequest
    - 댓글의 부모 댓글 id  element 추가
    - 기존 일반생성자  method 로 직접 instance 생성 부분을 of() method 를 통해  method 내부에서도 간접적으로 생성자를 호출하도록 수정
    
      ArticleCommentResponse
     - 댓글의 부모댓글 id element 추가
     - 자식 댓글 Set<ArticleCommentResponse>  type 추가
     - View 를 위한 응답의 경우, 화면에서 보여지는 댓글들이 정렬되어 있어야 한다.  따라서, 자식 댓글들 set 의 경우, 작성일 오름차순 & 댓글id 내림차순으로 정렬을 하고, 이를 TreeSet 에 담음.
    
     ArticleWithCommetsResponse
     - 여기에서는 댓글과 대댓글을 구분해 줘야한다. 이 작업을 위해 organizeChildComments method 를 추가하였음.
      organizeChildComments Logic
     1. ArticleWithCommentsDto 에 담긴 모든 articleCommentDto 들을 key - value map 으로 정리한다.
     2. 각 comment 중 부모가 있는(자식 댓글) 을 ArticleCommetResponse 의 childcomment Set 에 모두 담는다.
     3. 이렇게 자식 댓글을 모두 부모 댓글에 넣은 부모 댓글들만(root 댓글 모두) 다시 모아서 Set<ArticleCommetResponse> 로 담는다.
    
     ArticleCommentService
     - 댓글을 저장할때,  root 댓글이면 기존과 같이 그냥 저장하지만, 대댓글의 경우는, 부모 댓글 id 를 가져와서 부모 댓글의 자식으로 add 하도록 변경한다.
    
    ArticleCommentControllerTest / ArticleCommentServiceTest
    - 추가된 대댓글에 대한 처리가 되는디 해당 내용을 수정, 추가
    
    ArticleWithCommentsResponseTest
    - 기존 DTO 들은 생성, 및 DTO - Entity 변환 의 단순 작업만 포함하고 있었으나, ArticleWithCommentsResponse 의 경우 이번에 댓글과 대댓글을 분리하고 대댓글을 부부 댓글의 childComments Set 에 추가 시키는 logic 이 추가됨에 따라 관련 test 를 작성 , 의도대로 동작함을 확인함.

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
@@ -2,8 +2,10 @@ package com.fastcampus.projectboard.dto.response;
 
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * DTO for {@link com.fastcampus.projectboard.domain.ArticleComment}
@@ -14,16 +16,54 @@ public record ArticleCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        String userId
-) implements Serializable {
-    public static ArticleCommentResponse of(Long id,
-                                            String content,
-                                            LocalDateTime createdAt,
-                                            String email,
-                                            String nickname,
-                                            String userId
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
+) {
+    public static ArticleCommentResponse of(
+            Long id,
+            String content,
+            LocalDateTime createdAt,
+            String email,
+            String nickname,
+            String userId
     ) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
+        return ArticleCommentResponse.of(
+                id,
+                content,
+                createdAt,
+                email,
+                nickname,
+                userId,
+                null
+        );
+    }
+
+    public static ArticleCommentResponse of(
+            Long id,
+            String content,
+            LocalDateTime createdAt,
+            String email,
+            String nickname,
+            String userId,
+            Long parentCommentId
+    ) {
+        // view 에서 작성일 순으로 보여주기 위해 정렬 가능 집합 (TreeSet) 을 사용
+        // 해당 정렬을 위해 Comparator 를 사용
+        // 2차 정렬 조건으로 id 를 추가함.
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(
+                id,
+                content,
+                createdAt,
+                email,
+                nickname,
+                userId,
+                parentCommentId,
+                new TreeSet<>(childCommentComparator)
+        );
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -32,13 +72,18 @@ public record ArticleCommentResponse(
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of(
                 dto.id(),
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
-                dto.userAccountDto().userId()
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
         );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
     }
 }
