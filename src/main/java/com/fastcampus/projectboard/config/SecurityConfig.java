0a358e285e81ca14007bdb851d6234e0c4afe22c
gon <y2gon2@gmail.com>
2023-10-09 17:08:46 +0900

    #61 -  Spring Security OAuth2 적용 설정 methdo bean 추가 및 수정
    SecurityConfig
    1.  SecurityFilterChain
         -  기존 method chain 을 lamba 표현식으로 대체
         -  OAuth  login 조건이 추가되었으므로, 해당 조건 logout  상황 추가
    
    2. userDetailsService
        -  Spring Security  를  통과하기 위해 필요한 UserDetailsService 객제 정보를 얻기 위해 기존에는 userAccountRepository 에서 id 를 찾아 와서 현재 사용자 id 와 맞는지 확인하는 방법을 사용하였으나,  사용자 계정 data 접근에 필요한 business logic 이 추가됨에 따라  userAccountService 가 추가 되었다. 따라서 해당 접근을 UserAccountService.searchUser method 를 동해 얻는 구조로 수정함
    
    3. oAuth2UserService
        - Spring Secrurity OAuth2 가 제공하는  DefaultOAuth2UserService  객체를 통해 provider 가 보낸  인증 응답을 받을 객체르 미리 생성해 놓고,  provider 에게 요청을 넣었을 때, 해당 정보를 받아낸다 (OAuth2User)
        - provider 별로 data 구조, key  가 다를 수 있으므로 이를 제대로 deserialize 할수 있도록 미리 작성해 놓은 KakaoOAuth2Response record 에 해당 delegate 정보를 넣어서 의도한 대로 data를 의미 있는 구조로 변환한다.
        -  응답 받은 value 를 사용하여 id / pw 를 생성한다. (pw 는 사실 의미없지만, 기존 개발과정에서 사용한 구조를 그대로 사용하기 위해 생성)
       -  최조 OAuth 인증 이후부터 DB 저장된 인증 정보를 사용할 것이므로,  id 를 기존 DB 정보에 있는지 확인하고, 없다면 DB 에 저장을 진행한다.
        - 위 business logic 은 최대한 lambda 표현식을 사용하여 작성하였다.
        - 특히 해당 method 전체 code 가 사실  하나의 lambda 식 내부에서 구현되었으며 이는 ,  반환 type 인 OAuth2UserSerive type 이 functional interface 로 loadUser method 의 구현체 이다.  해당 method signiture 는 R input , U return generic 이다. 각 generic 에 대한 type 정의는 각각 <OAuth2UserRequest, OAuth2User> 으로 명시되었다.

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -1,25 +1,37 @@
 package com.fastcampus.projectboard.config;
 
 import com.fastcampus.projectboard.dto.security.BoardPrincipal;
+import com.fastcampus.projectboard.dto.security.KakaoOAuth2Response;
 import com.fastcampus.projectboard.repository.UserAccountRepository;
 import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
 
 @Configuration
 public class SecurityConfig {
 
     // secruity 에 태워서 security 에 관리하에 두고 인증과 권한 체크 진행
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+    ) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // static resource 에 대해서 "인증" 을 검사 없이 모두 허가
@@ -31,10 +43,13 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin().and()
-                .logout()
-                    .logoutSuccessUrl("/")
-                    .and()
+                .formLogin(Customizer.withDefaults()) // 기본값을 내부적으로 설정. 따라서 method chaining 을 위한 and() 가 필요 없어짐.
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oAuth -> oAuth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)
+                        )
+                )
                 .build();
     }
 
@@ -65,19 +80,81 @@ public class SecurityConfig {
     // 암호와 module 을 사용할 수 있게 된다.
 
     @Bean
-    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
         // username -> id -> userAccountDto -> BoardPrincipal
         // UserDetailsService interface method 인 loadUserByUsername 을 아래와 같이 구현한 것임.
-        return username -> userAccountRepository
-                .findById(username)
-                .map(UserAccountDto::from)
+        return username -> userAccountService
+                .searchUser(username)
                 .map(BoardPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - username " + username));
     }
 
     @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            UserAccountService userAccountService,
+            PasswordEncoder passwordEncoder
+    ) {
+        // OAuth2.0 REST API provider 정보를 받아주는 SpringSecrurity OAuth2 기능을 사용할 변수 선언
+        // spring security OAuth 기본 구현체
+        // bean 으로 자동 등록 X -> 기본적으로 parameter 로 Injection 하여 사용하지 않고 생성자로 직접 만들어 줘야
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        // OAuth2UserService interface 는 아래 method 를 가진 functionalInterface 이다.
+        // U loadUser(R userRequest) throws OAuth2AuthenticationException;
+        // 따라서 아래 lambda 식은 loadUser method 의 구현체 이다.
+        // lambda 표현식 parameter 는 해당 구현체의 parameter 인 userRequest 를 말하는 것이며
+        // 해당 fucntion interface 에 사용된 generic R U 에 대해서는 현재 method 반환 type <> 에
+        // 각각 OAuth2UserRequest, OAuth2User 로 정의되어 있으므로
+        // userRequest 는 곧 R : OAuth2UserRequest 임을 알 수 있다.
+        return userRequest -> {
+            // Spring Security OAuth2 서비스를 통해 provider 가 응답한 사용자 정보를 load
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            // provider 마다 parsing 구조, 내요이 다르므로 이를 정의한 class 를 통해 유의미한 형태로 encoding 한 data 생성
+            KakaoOAuth2Response kakaoResponse = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            // 사용자 이름은 에초에 OAuth API 로 가져올수 없기 때문에 우리가 생성해줘야 함 (?)
+
+            // yaml 파일에서 security.oauth2.client.registration 으로 설정했던 provider (kakao) 를 의미
+            String registrationId = userRequest.getClientRegistration().getRegistrationId(); // "kakao"
+            String providerId = String.valueOf(kakaoResponse.id());
+            String username = registrationId + "_" + providerId;
+            // 회원 여부 인증은 kakao 에서 처리하므로 사실 password 는 필요가 없다.
+            // 다만 해당 project 에서는 최초 password 가 설계에 포함되었었기 때문에 이를 고려하여
+            // 수정하지 않고 dummy password 를 넣어줌.
+            String dummyPassword = passwordEncoder.encode("{bcrypt}dummy" + UUID.randomUUID());
+
+            // DB 에 userAccount 존재 -> Ok 아니면 저장
+            return userAccountService.searchUser(username)
+                    .map(BoardPrincipal::from) // 있다면 인증 정보 생성 내보냄.
+                    .orElseGet(() ->
+                            BoardPrincipal.from( // 없다면 생성하여 내보냄.
+                                    userAccountService.saveUser(
+                                            username,
+                                            dummyPassword,
+                                            kakaoResponse.email(),
+                                            kakaoResponse.nickname(),
+                                            null
+                                    )
+                            )
+                    );
+        };
+    }
+
+    // 앞에 method 에서 PasswordEncoder 를 DI 적용하기 위한 method 이다.
+    // Spring Security 에 정의된 PasswordEncoder interface 를 반환타입으로 정의하여
+    // @Bean method 로 정의하여, Spring ApplicationContext 에 Bean 으로 등록 시킴으로써
+    // DI 적용이 가능해진다.
+    // (config class 내 method 대부분 사용자 정의 또는 DI 적용을 위한 bean method 들을 정의하는 곳 이였음. )
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        // passwordEncoder 설정을 위임해서 factory 로 부터 가져오겠다.
+        // PasswordEnoderFactories.createDelegatingPasswordEncoder()
+        // 여러가지 비밀번호 인코딩 방식을 지원하기 위한 PasswordEncoder 의 구현체인
+        // DelegatingPasswordEncoder 를 생성함.
+        // createDelegatingPasswordEncoder의 주요 특징
+        // 1. 다양한 인코딩 방식 지원: 이 메서드를 통해 생성된 DelegatingPasswordEncoder는 여러 인코딩 방식(예: bcrypt, scrypt, pbkdf2 등)을 지원
+        // 2. 접두어를 사용한 인코딩 식별: 저장된 비밀번호 값 앞에 {...} 형식의 접두어를 붙여, 해당 비밀번호가 어떤 인코딩 방식으로 인코딩되었는지 식별.
+        //    예를 들어, {bcrypt}... 접두어는 비밀번호가 bcrypt 방식으로 인코딩되었음을 나타냄.
+        //    기본적으로 bcrypt 방식을 사용하여 비밀번호를 인코딩
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
