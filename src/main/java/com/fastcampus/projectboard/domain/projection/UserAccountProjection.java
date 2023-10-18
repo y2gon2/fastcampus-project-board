2e94d684adda83ab54d2f4af11ecfb5d387cf930
gon <y2gon2@gmail.com>
2023-10-18 15:00:14 +0900

    #104 - Data REST Projection 을 이용한 data 노출 Field 설정
    기본 설정 (@RepositoryRestResource) 으로 Data REST API 접근을 했을 경우 RESTful 한 설정에 맞게 제공되는 JSON 에서는 API link 로 제공된다. 이는 data 를 사용하는 관리자 프로젝트에서 하나의 관련 data 를 모두 받기 위해 복수의 API 요청을 진행해야 하는 문제가 발생할 수 있다.
    이를 해결하기 위해 각 endpoint 에서 Projection 을 설정하여, REST API 요청에서 요청되는 Field 를 바로 노출시키는 설정을 만들 수 있다. 추가로 해당 Projection 을 사용하면, 비밀번호와 같은 민감 정보는 노출 대상에서 제외 시켜 REST API 로 접근하지 못하도록 설정도 가능하다.

---
+++ src/main/java/com/fastcampus/projectboard/domain/projection/UserAccountProjection.java
@@ -0,0 +1,20 @@
+package com.fastcampus.projectboard.domain.projection;
+
+
+import com.fastcampus.projectboard.domain.UserAccount;
+import net.bytebuddy.asm.Advice;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDateTime;
+
+@Projection(name = "withoutPassword", types = UserAccount.class)
+public interface UserAccountProjection {
+    String getUserId();
+    String getEmail();
+    String getNickname();
+    String getMemo();
+    LocalDateTime getCreatedAt();
+    String getCreatedBy();
+    LocalDateTime getModifiedAt();
+    String getModifiedBy();
+}
